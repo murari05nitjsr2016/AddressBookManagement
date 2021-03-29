35aaa295ee888c67cb15ff1a3ae196c327af10fc
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-29 22:08:44 +0530

    Welcome File

---
+++ src/com/addressbookmgmt/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.addressbookmgmt;
+
+public class AddressBookMain {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Address Book Program");
+
+	}
+
+
+}
